171819df45697281d4afa13d1cb851170cb4d809
Kovács, Péter <peter.dunay.kovacs@gmail.com>
2018-10-22 13:37:46 +0200

    Fix logged message format

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { format } from "util";
 import * as path from "path";
 
 import { LoggerFactory } from "./logger";
@@ -5,6 +6,8 @@ import * as rxUtils from "./rx-utils";
 import { RxGoogleAPI } from "./rx-google-api";
 import { catchError, map, flatMap } from "rxjs/operators";
 import { Observable } from "rxjs";
+
+const LISTING_BATCHE_SIZE = 1;
 
 export default (loggerFactory: LoggerFactory, rxGapi: RxGoogleAPI) => {
     const logger = loggerFactory("index");
@@ -18,8 +21,8 @@ export default (loggerFactory: LoggerFactory, rxGapi: RxGoogleAPI) => {
         }),
         // Authorize a client with credentials, then call the Google Drive API.
         flatMap(credentials => rxGapi.authorize(JSON.parse(credentials))),
-        flatMap(oAuth2Client => rxGapi.listDriveFilesAll(oAuth2Client, 3)),
-        map(file => logger.info(`file: ${file}`))
+        flatMap(oAuth2Client => rxGapi.listDriveFilesAll(oAuth2Client, LISTING_BATCHE_SIZE)),
+        map(file => logger.info(format("file: %O", file)))
     );
 
 };
